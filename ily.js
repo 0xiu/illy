dd1992bf583faf4004e2f83c294a3171f15b46ed
OwO 3 <98687363+xcrud@users.noreply.github.com>
2022-05-19 19:23:45 +0700

    uwu

---
+++ ily.js
@@ -0,0 +1,19 @@
+L2Dwidget.init({
+        "model": {                                                          jsonPath: "https://unpkg.com/live2d-widget-model-miku@1.0.5/assets/miku.model.json",
+            "scale": 1                                                  },
+        "display": {
+            "position": "right",                                            "width": 500,
+            "height": 800,
+            "hOffset": 0,
+            "vOffset": -20
+        },
+        "mobile": {
+            "show": true,
+            "scale": 0.5
+        },
+        "react": {
+            "opacityDefault": 0.7,
+            "opacityOnHover": 0.2
+        }                                                           
+        
+});
